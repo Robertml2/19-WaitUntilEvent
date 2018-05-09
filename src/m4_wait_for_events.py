f18f475c5c35e706122b11ec155c58f5674ff1e3
robertml2 <robertm2@rose-hulman.edu>
2018-05-09 05:34:43 -0400

    Commit

--- src/m4_wait_for_events.py
+++ src/m4_wait_for_events.py
@@ -91,7 +91,7 @@ def sum_until_prime_input():
          The sum of the input integers is: 167
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #   The testing code is already written for you (above).
     # ------------------------------------------------------------------
     total = 0
@@ -100,7 +100,7 @@ def sum_until_prime_input():
         total = total + x
         if is_prime(x):
             break
-    return total
+    print("The sum id the input integers is:",total)
 
 
 
@@ -200,8 +200,9 @@ def next_prime(m):
     # IMPLEMENTATION REQUIREMENT:
     #    -- Use (call) the   is_prime   function above appropriately.
     # ------------------------------------------------------------------
-    #for k in range(m):
-       # if
+    while True:
+        x = int(input("Enter an integer greater than 1"))
+    
 
 
 
